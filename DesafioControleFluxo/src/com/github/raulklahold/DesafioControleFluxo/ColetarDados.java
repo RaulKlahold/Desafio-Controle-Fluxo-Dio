9ded3a609ebeb828b549d89f09f76b09cf6c83a9
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-04 22:57:58 -0300

    alteração dos calc

--- DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/ColetarDados.java
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/ColetarDados.java
@@ -3,18 +3,31 @@ package com.github.raulklahold.DesafioControleFluxo;
 public class ColetarDados {
 	private int parametroUM;
 	private int parametroDois;
+	private FunctionAndCalc valorretornado;
+	
+
+	public FunctionAndCalc getValorretornado() {
+		return valorretornado;
+	}
+
+	public void setValorretornado(FunctionAndCalc valorretornado) {
+		this.valorretornado = valorretornado;
+	}
+
 	public int getParametroUM() {
 		return parametroUM;
 	}
+
 	public void setParametroUM(int parametroUM) {
 		this.parametroUM = parametroUM;
 	}
+
 	public int getParametroDois() {
 		return parametroDois;
 	}
+
 	public void setParametroDois(int parametroDois) {
 		this.parametroDois = parametroDois;
 	}
-	
 
 }
--- DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/FunctionAndCalc.java
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/FunctionAndCalc.java
@@ -1,5 +1,17 @@
 package com.github.raulklahold.DesafioControleFluxo;
 
 public class FunctionAndCalc {
-
+	public static int contar(ColetarDados ColetarDados ){
+		
+		int numeroUm = ColetarDados.getParametroUM();
+		int numeroDois = ColetarDados.getParametroDois();
+		int cont = numeroDois - numeroUm;
+		
+		for (int i = 0; i <= cont; i++) {
+            System.out.println(i);
+        }
+		return cont;
+		
+		
+	}
 }
