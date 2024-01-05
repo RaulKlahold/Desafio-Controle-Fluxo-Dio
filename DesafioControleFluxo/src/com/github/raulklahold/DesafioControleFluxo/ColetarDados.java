9428a014980ef34d5ce6bbed4682d20a93ae2553
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-04 22:35:19 -0300

    commitando getters e setters

--- DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/ColetarDados.java
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/ColetarDados.java
@@ -1,5 +1,20 @@
 package com.github.raulklahold.DesafioControleFluxo;
 
 public class ColetarDados {
+	private int parametroUM;
+	private int parametroDois;
+	public int getParametroUM() {
+		return parametroUM;
+	}
+	public void setParametroUM(int parametroUM) {
+		this.parametroUM = parametroUM;
+	}
+	public int getParametroDois() {
+		return parametroDois;
+	}
+	public void setParametroDois(int parametroDois) {
+		this.parametroDois = parametroDois;
+	}
+	
 
 }
---
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/RetornarMain.java
@@ -0,0 +1,10 @@
+package com.github.raulklahold.DesafioControleFluxo;
+
+public class RetornarMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
