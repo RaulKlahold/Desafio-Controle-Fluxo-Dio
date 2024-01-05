ba0eb81890385591f8251da62d70b51cd83a8d5f
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-04 23:31:49 -0300

    versão final do projeto

--- DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/Exceptions.java
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/Exceptions.java
@@ -3,12 +3,11 @@ package com.github.raulklahold.DesafioControleFluxo;
 @SuppressWarnings("serial")
 public class Exceptions extends Exception {
 
-   
 	public Exceptions() {
-        super("O segundo parâmetro deve ser maior que o primeiro");
-    }
+		super("O segundo parâmetro deve ser maior que o primeiro");
+	}
 
-    public Exceptions(String message) {
-        super(message);
-    }
+	public Exceptions(String message) {
+		super(message);
+	}
 }
--- DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/RetornarMain.java
+++ DesafioControleFluxo/src/com/github/raulklahold/DesafioControleFluxo/RetornarMain.java
@@ -1,9 +1,27 @@
 package com.github.raulklahold.DesafioControleFluxo;
 
+import java.util.Scanner;
+
 public class RetornarMain {
 
 	public static void main(String[] args) {
-		
+		Scanner Scan = new Scanner(System.in);
+		ColetarDados calc = new ColetarDados();
+
+		System.out.println("Digite um numero inteiro: ");
+		int numeroU = Scan.nextInt();
+		calc.setParametroUM(numeroU);
+
+		System.out.println("Digite um segundo numero inteiro: ");
+		int numeroD = Scan.nextInt();
+		calc.setParametroDois(numeroD);
+		Scan.close();
+
+		try {
+			FunctionAndCalc.contar(calc);
+		} catch (Exceptions e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
